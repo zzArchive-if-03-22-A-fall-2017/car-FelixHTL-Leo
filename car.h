ea84ee2e6ce900a2ffc4d1305b2694d8fd62a0c0
Felix Bogengruber <felixbogi@gmail.com>
2017-11-24 12:03:32 +0100

    first test

--- car.h
+++ car.h
@@ -13,4 +13,39 @@
 #ifndef ___CAR_H
 #define ___CAR_H
 
+enum Type{
+  AIXAM,
+  FIAT_MULTIPLA,
+  JEEP
+};
+
+enum Color{
+  RED,
+  GREEN,
+  BLUE,
+  ORANGE,
+  SILVER,
+  BLACK
+};
+
+typedef struct CarType* Car;
+
+void init();
+
+int get_speed(Car car);
+
+Type get_type(Car car);
+
+Color get_color(Car car);
+
+double get_fill_level(Car car);
+
+double get_acceleration_rate(Car car);
+
+Car get_car(Type type);
+
+double set_acceleration_rate(Car car, double acceleration);
+
+void accelerate(Car car);
+
 #endif
